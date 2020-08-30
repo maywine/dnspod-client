9f371160930aae6771812979973e0418edd7d1c7
maple <maplewine@outlook.com>
2020-08-31 00:32:28 +0800

    add code

--- dnspod.cpp
+++ dnspod.cpp
@@ -8,68 +8,76 @@
 #include <map>
 #include <set>
 #include <fstream>
+#include <thread>
+#include <chrono>
+#include <regex>
 
-#include <boost/asio.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/ssl.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/ssl/error.hpp>
-#include <boost/asio/ssl/stream.hpp>
-
+#include <boost/lexical_cast.hpp>
 #include <nlohmann/json.hpp>
 
-struct do_on_exit
-{
-    do_on_exit(std::function<void(void)> hd) : do_on_exit_hd_(hd) {}
-    ~do_on_exit()
-    {
-        if (do_on_exit_hd_)
-        {
-            do_on_exit_hd_();
-        }
-    }
+static std::string get_current_time();
 
-private:
-    std::function<void(void)> do_on_exit_hd_;
-};
+#define LOG_MSG(fmt, args...) fprintf(stderr, "%s: " fmt "\n", get_current_time().c_str(), ## args)
 
 struct record_info
 {
-    uint32_t record_id = 0;
-    std::string record_name;
+    std::string record_id = 0;
+    std::string record_sub_domain;
     std::string record_type;
     std::string record_value;
-    std::string enabled;
+    std::string ttl;
     inline bool operator<(const record_info &other) const
     {
-        return record_id < other.record_id || record_value < other.record_value;
+        return record_id < other.record_id || record_sub_domain < other.record_sub_domain || record_type < other.record_type || record_value < other.record_value;
     }
 };
 
 struct domain_info
 {
-    uint32_t domain_id = 0;
-    uint32_t records = 0;
+    std::string domain_id = 0;
     std::string domain;
-    std::set<record_info> record_info_vec;
+    std::set<record_info> record_info_set;
+    std::vector<record_info> record_info_vec;
 };
+
+static std::string GetValue(const nlohmann::json &js,
+                            const std::string &key,
+                            const char *default_value);
 
 static bool http_sync_request(const boost::beast::http::verb method,
                               const std::string &host,
-                              const std::string &port = "443",
-                              const std::string &path = "/",
-                              const std::string &request = "",
-                              const std::map<boost::beast::http::field, std::string> &header = std::map<boost::beast::http::field, std::string>(),
-                              std::string *resp = nullptr);
+                              const std::string &port,
+                              const std::string &path,
+                              const std::string &request,
+                              const std::map<boost::beast::http::field, std::string> *header,
+                              std::string *resp);
+
+static bool dnspod_api(const std::string &path,
+                       const std::string &request,
+                       nlohmann::json &resp_js);
+
+static std::string get_current_ip();
+
+static void get_domain_list(std::map<std::string, domain_info> &domain_info_map);
+
+static bool update_dns_record(const std::string &domain_id,
+                              const std::string &record_id,
+                              const std::string &sub_domain,
+                              const std::string &record_tpye,
+                              const std::string &ip,
+                              const std::string &ttl);
+
+static void update_dns_loop(const nlohmann::json &domain_list_js,
+                            std::map<std::string, domain_info> &domain_info_map);
+
+static const std::string s_g_dnspod_api_host = "dnsapi.cn";
+static std::string s_g_dnspod_token = "";
 
 int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        fprintf(stderr, "the config path must set\n");
+        LOG_MSG("the config path must set");
         return 1;
     }
 
@@ -78,18 +86,173 @@ int main(int argc, char **argv)
         std::ifstream ifs(argv[1]);
         if (!ifs)
         {
-            fprintf(stderr, "open config: %s failed\n", argv[1]);
+            LOG_MSG("open config: %s failed", argv[1]);
             return 1;
         }
 
         nlohmann::json config_json;
         ifs >> config_json;
-        
-        std::map<std::string, domain_info> domain_info_map;
-    }
-    catch(const std::exception &err)
-    {
 
+        s_g_dnspod_token = GetValue(config_json, "token", "");
+        if (s_g_dnspod_token.empty())
+        {
+            LOG_MSG("token is empty");
+            return 1;
+        }
+
+        auto domain_it = config_json.find("domain_list");
+        if (domain_it == config_json.end() || !domain_it->is_array())
+        {
+            LOG_MSG("domain_list invalid");
+            return 1;
+        }
+
+        std::map<std::string, domain_info> domain_info_map;
+        get_domain_list(domain_info_map);
+        if (domain_info_map.empty())
+        {
+            return 1;
+        }
+
+        update_dns_loop(*domain_it, domain_info_map);
+    }
+    catch (const std::exception &err)
+    {
+        LOG_MSG("main, catch exception:%s", err.what());
+        return 1;
+    }
+}
+
+static void update_dns_loop(const nlohmann::json &domain_list_js, std::map<std::string, domain_info> &domain_info_map)
+{
+    try
+    {
+        uint64_t count = 0;
+        std::string current_ip;
+        std::regex ip_reg("((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}");
+
+        while (true)
+        {
+            if (count % (15 * 60 / 5) == 0) // 15 mim get ip from dnspod
+            {
+                domain_info_map.clear();
+                while (true)
+                {
+                    get_domain_list(domain_info_map);
+                    if (domain_info_map.empty())
+                    {
+                        LOG_MSG("try domain list failed");
+                        std::this_thread::sleep_for(std::chrono::seconds(10));
+                        continue;
+                    }
+
+                    for (auto &item : domain_info_map)
+                    {
+                        item.second.record_info_vec.reserve(item.second.record_info_set.size());
+                        std::copy(item.second.record_info_set.begin(), item.second.record_info_set.end(), std::back_inserter(item.second.record_info_vec));
+                    }
+
+                    break;
+                }
+            }
+
+            current_ip = get_current_ip();
+            if (std::regex_match(current_ip, ip_reg))
+            {
+                for (auto &item : domain_list_js)
+                {
+                    if (!item.is_object())
+                    {
+                        LOG_MSG("invalid domain_list: %s", domain_list_js.dump().c_str());
+                        exit(1);
+                    }
+
+                    auto domain = GetValue(item, "domain", "");
+                    auto sub_domain = GetValue(item, "sub_domain", "");
+                    if (domain.empty() || sub_domain.empty())
+                    {
+                        LOG_MSG("invalid domain_list: %s", domain_list_js.dump().c_str());
+                        exit(1);
+                    }
+                    auto ttl = GetValue(item, "ttl", "600");
+
+                    auto it = domain_info_map.find(domain);
+                    if (it == domain_info_map.end())
+                    {
+                        LOG_MSG("cann't find domain: %s at dnspod list", domain.c_str());
+                        exit(1);
+                    }
+
+                    // update dns record
+                    for (auto &record : it->second.record_info_vec)
+                    {
+                        if (record.record_id.empty() || record.record_sub_domain.empty())
+                        {
+                            continue;
+                        }
+
+                        // Last IP is the same as current IP
+                        if (current_ip == record.record_value)
+                        {
+                            continue;
+                        }
+
+                        // update dns record
+                        if (update_dns_record(it->second.domain_id, record.record_id, sub_domain, record.record_type, current_ip, ttl))
+                        {
+                            record.record_value = current_ip;
+                        }
+                    }
+                }
+            }
+
+            ++count;
+            std::this_thread::sleep_for(std::chrono::seconds(15));
+        }
+    }
+    catch (const std::exception &err)
+    {
+    }
+}
+
+template <typename T>
+static T GetValue(const nlohmann::json &js, const std::string &key, const T &default_value)
+{
+    auto it = js.find(key);
+    if (it == js.end())
+    {
+        return default_value;
+    }
+
+    try
+    {
+        return boost::lexical_cast<T>(it.value());
+    }
+    catch (...)
+    {
+        return default_value;
+    }
+}
+
+static std::string GetValue(const nlohmann::json &js, const std::string &key, const char *default_value)
+{
+    auto it = js.find(key);
+    if (it == js.end())
+    {
+        return std::string(default_value);
+    }
+
+    try
+    {
+        if (it->is_string())
+        {
+            return it.value();
+        }
+        return boost::lexical_cast<std::string>(it.value());
+    }
+    catch (...)
+    {
+        return std::string(default_value);
     }
 }
 
@@ -98,7 +261,7 @@ bool http_sync_request(const boost::beast::http::verb method,
                        const std::string &port,
                        const std::string &path,
                        const std::string &request,
-                       const std::map<boost::beast::http::field, std::string> &header,
+                       const std::map<boost::beast::http::field, std::string> *header,
                        std::string *resp)
 {
     namespace beast = boost::beast; // from <boost/beast.hpp>
@@ -126,12 +289,17 @@ bool http_sync_request(const boost::beast::http::verb method,
         auto const results = resolver.resolve(host, port);
         beast::get_lowest_layer(stream).connect(results);
         stream.handshake(ssl::stream_base::client);
-        http::request<http::string_body> req{method, path, 11, request};
+        http::request<http::string_body> req{method, path, 11};
         req.set(http::field::host, host);
         req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-        for (auto &item : header)
+        req.body() = request;
+        req.prepare_payload();
+        if (header)
         {
-            req.set(item.first, item.second);
+            for (auto &item : *header)
+            {
+                req.set(item.first, item.second);
+            }
         }
 
         http::write(stream, req);
@@ -156,10 +324,164 @@ bool http_sync_request(const boost::beast::http::verb method,
             throw beast::system_error{ec};
         }
     }
-    catch (const std::exception &err)
+    catch (...)
     {
-        return false;
     }
 
     return true;
+}
+
+static bool dnspod_api(const std::string &path,
+                       const std::string &request,
+                       nlohmann::json &resp_js)
+{
+    try
+    {
+        static const std::string agent = "AnripDdns/6.0.0(mail@anrip.com)";
+        std::map<boost::beast::http::field, std::string> header;
+        header.emplace(boost::beast::http::field::user_agent, agent);
+        std::string resp;
+        http_sync_request(boost::beast::http::verb::post, s_g_dnspod_api_host, "443", path, request, &header, &resp);
+        std::cout << resp << std::endl;
+        resp_js = nlohmann::json::parse(resp);
+        auto it = resp_js.find("status");
+        if (it == resp_js.end() || !it->object())
+        {
+            LOG_MSG("dnspod_api failed, invalid resp: %s", resp.c_str());
+            return false;
+        }
+
+        if (GetValue(*it, "code", -1) != 1)
+        {
+            LOG_MSG("dnspod_api failed, resp: %s", resp.c_str());
+            return false;
+        }
+        else 
+        {
+            return true;
+        }
+    }
+    catch (const std::exception &err)
+    {
+        LOG_MSG("dnspod_api failed, catch exception:%s", err.what());
+        return false;
+    }
+
+}
+
+static std::string get_current_ip()
+{
+    std::string host_ip;
+    http_sync_request(boost::beast::http::verb::get, "ifconfig.me", "443", "/ip", "", nullptr, &host_ip);
+    return host_ip;
+}
+
+static void get_domain_list(std::map<std::string, domain_info> &domain_info_map)
+{
+    try
+    {
+        nlohmann::json req_js;
+        req_js["login_token"] = s_g_dnspod_token;
+        req_js["format"] = "json";
+        nlohmann::json resp_js;
+        std::cout << req_js.dump() << std::endl;
+        if (!dnspod_api("/Domain.List", req_js.dump(), resp_js)) //Domain.List
+        {
+            LOG_MSG("get domain list failed");
+            return;
+        }
+
+        auto it = resp_js.find("domains");
+        if (it != resp_js.end() && it->is_array())
+        {
+            for (auto &item : *it)
+            {
+                if (!item.is_object())
+                {
+                    continue;
+                }
+
+                domain_info domain;
+                auto domain_str = GetValue(item, "name", "");
+                domain.domain = domain_str;
+                domain.domain_id = GetValue(item, "id", "");
+                if (domain.domain.empty() || domain.domain_id.empty())
+                {
+                    continue;
+                }
+
+                req_js["domain_id"] = domain.domain_id;
+                nlohmann::json resp_js;
+                if (!dnspod_api("/Record.List", req_js.dump(), resp_js)) //Record.List
+                {
+                    LOG_MSG("get domain: %s record list failed", domain_str.c_str());
+                    continue;
+                }
+
+                auto it = resp_js.find("records");
+                if (it != resp_js.end() && it->is_array())
+                {
+                    for (auto &item : *it)
+                    {
+                        if (!item.is_object())
+                        {
+                            continue;
+                        }
+
+                        record_info record;
+                        record.record_id = GetValue(item, "id", "");
+                        record.record_sub_domain = GetValue(item, "name", ""); //@, www,
+                        record.record_type = GetValue(item, "type", "A");      //A, AAAA
+                        record.record_value = GetValue(item, "value", "");     // IP
+                        record.ttl = GetValue(item, "ttl", "");                // ttl
+                        domain.record_info_set.insert(std::move(record));
+                    }
+                }
+
+                domain_info_map.emplace(std::move(domain_str), std::move(domain));
+            }
+        }
+    }
+    catch (const std::exception &err)
+    {
+        LOG_MSG("get_domain_list failed, catch exception:%s", err.what());
+    }
+}
+
+static bool update_dns_record(const std::string &domain_id,
+                              const std::string &record_id,
+                              const std::string &sub_domain,
+                              const std::string &record_tpye,
+                              const std::string &ip,
+                              const std::string &ttl)
+{
+    nlohmann::json req_js;
+    req_js["login_token"] = s_g_dnspod_token;
+    req_js["format"] = "json";
+    req_js["domain_id"] = domain_id;
+    req_js["record_id"] = record_id;
+    req_js["sub_domain"] = sub_domain;
+    req_js["record_type"] = record_tpye;
+    req_js["value"] = ip;
+    req_js["ttl"] = ttl;
+    nlohmann::json resp_js;
+    if (!dnspod_api("/Record.Modify", req_js.dump(), resp_js))
+    {
+        LOG_MSG("update_dns_record failed");
+        return false;
+    }
+    return true;
+}
+
+static std::string get_current_time()
+{
+    try
+    {
+        auto now = boost::posix_time::second_clock::local_time();
+        return boost::posix_time::to_simple_string(now);
+    }
+    catch (...)
+    {
+        return "";
+    }
 }
